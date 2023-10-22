d0a4c35b6bf9165be4f3833c528260b66043dce6
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-23 00:15:51 +0700

    Segitiga siku siku
    Segitiga siku siku

---
+++ Tugas15.js
@@ -0,0 +1,9 @@
+var AB = 8;
+var BC = 6;
+var AC = Math.pow(AB,2)+Math.pow(BC,2);
+var hasil = Math.sqrt(100);
+
+console.log("Nilai AB = " +AB);
+console.log("Nilai BC = " +BC);
+console.log(AC);
+console.log("Panjang sisi AC pada segitiga siku - siku tersebut adalah : "+hasil);
